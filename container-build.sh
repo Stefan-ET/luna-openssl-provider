53d3eda6d0dc5884694b29c6f6fce167bbbc8be0
Eli Waltuch <eli.waltuch@thalesgroup.com>
2025-06-11 08:20:07 +0000

    Containerized build for luna:
    Added help message and improved arg parsing
    
    Signed-off-by: Eli Waltuch <eli.waltuch@thalesgroup.com>

--- container-build.sh
+++ container-build.sh
@@ -16,6 +16,15 @@ LIBOQS_VERSION="0.12.0"
 LIBOQS_TAR="$LIBOQS_VERSION.tar.gz"
 LIBOQS_URL="https://github.com/open-quantum-safe/liboqs/archive/refs/tags/$LIBOQS_TAR"
 
+function usage() {
+	echo "Builds the Luna OpenSSL provider, Gem Engine, and sautil inside a Docker container."
+	echo
+	echo "Usage: $0 [--rebuild]"
+	echo "  --rebuild | -r : Quickly rebuild the Luna artifacts without downloading dependencies and rebuilding the Docker image."
+	echo "  --help | -h : Show this help message."
+	echo "  If not specified, the script will build the Docker image, download and build dependencies, and then build the Luna artifacts."
+}
+
 function download_deps() {
 	curl -o $(dirname $0)/openssl-source/$OPENSSL_TAR -L $OPENSSL_URL
 	curl -o $(dirname $0)/openssl-source/liboqs-$LIBOQS_TAR -L $LIBOQS_URL
@@ -37,11 +46,14 @@ function pack_tarball() {
 	tar -czf luna.tar.gz -C $(dirname $0) builds
 }
 
-# parse command line arguments
 case "$1" in
-    "--rebuild")
+    "rebuild" | "--rebuild" | "-r")
         CLEAN_BUILD=0
 	;;
+    "help" | "--help" | "-h")
+        usage
+        exit 1
+        ;;
     *)
         CLEAN_BUILD=1
         ;;
